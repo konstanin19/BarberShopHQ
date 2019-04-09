e965f9cdd8bb1063f83eb296fff35ae7a95336ee
Konstantin K <konstanin19@mail.ru>
2019-04-09 23:30:56 +0300

    Add error ActiveRecord

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'sinatra/activerecord'
 
 set :database, "sqlite3:barbershop.db"
 
-class Client < Activerecord::Base
+class Client < ActiveRecord::Base
 end
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
