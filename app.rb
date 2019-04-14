42891f23e529c3d6078935b64e3d7fcd5a939a44
Konstantin K <konstanin19@mail.ru>
2019-04-14 21:40:31 +0300

    Add gemfile new

--- app.rb
+++ app.rb
@@ -1,13 +1,12 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-set :database, "sqlite3:barbershop.db"
+#set :database, "sqlite3:barbershop.db"
 
-class Client < ActiveRecord::Base
-end
+#class Client < ActiveRecord::Base
+#end
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
