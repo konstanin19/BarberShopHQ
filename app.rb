2154d2cfce1632e254fff0ac09db6d389b0012d6
Konstantin K <konstanin19@mail.ru>
2019-04-18 21:45:59 +0300

    Add validation

--- app.rb
+++ app.rb
@@ -6,14 +6,16 @@ require 'IRB'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
+	validates :name, presence: true
+	validates :phone, presence: true
+	validates :datestamp, presence: true
+	validates :color, presence: true
+	
+
 end
 
 class Barber < ActiveRecord::Base
 end
-
-class Record < ActiveRecord::Base
-end
-
 
 before do
 @barbers = Barber.all
@@ -28,55 +30,19 @@ get '/visit' do
 end
 
 post '/visit' do
-	@username = params[:username]
-	@phone     = params[:phone]
-	@datetime = params[:datetime]
-	@baber = params[:baber]
-	@color = params[:color]
-
-	hh = { :username => 'Введите имя',
-       :phone => 'Введите номер телефона',
-       :datetime => 'Введите дату и время визита' }
-       @error = hh.select {|key,_| params[key] == ""}.values.join(",")
-       #для каждой пары ключ-значение
-    #   hh.each do |key, value|
-        #если параметр пуст
-     #          if  params[key] == ''
-                #переменной error присвоить значение value из хеша hh
-                #(а value из хеша hh это сообщение об ошибке)
-                #т.е. переменной error присвоить сообщение об ошибке
-    if @error != ''
-        return erb :visit
-    end
-
-    c = Client.new
-    c.name = @username
-    c.phone = @phone
-    c.datestamp = @datestamp
-	c.barber = @barber
-    c.color = @color
-    c.save
-
-      
+	c = Client.new params[:client]
+	c.save
 
 
-	
 
-    @title = 'Thank you!'
-	@message = "Дорогой #{@username}, вы записались #{@datetime} к парикмахеру #{@baber},цвет краски: #{@color}"
+	erb "<h3>Спасибо!
+	         Дорогой client[name], вы записались :datestamp к парикмахеру #{:barber},цвет краски: #{:color}</h3>"
 
-	#f = File.open 'users.txt', 'a'
-	#f.write "User: #{@username}, Phone: #{@phone}, Date and time: #{@datetime} ,Baber:#{@baber}, Color:#{@color}"
-	#f.close       		db = get_db
-
-
-	erb :message
 end
 
 
-get '/showusers' do
-       # db = get_db
 
-       # @results = db.execute 'select * from Clients order by id desc'
+get '/showusers' do
+  
  erb :showusers
 end
--- db/migrate/20190418162036_create_records.rb
+++ db/migrate/20190418162036_create_records.rb
@@ -1,4 +1,13 @@
 class CreateRecords < ActiveRecord::Migration[5.2]
   def change
+
+  	create_table :records do |t|
+      t.text :name
+      t.text :phone
+      t.text :datestamp
+      t.text :barber
+
+        t.timestamps
+    end
   end
 end
